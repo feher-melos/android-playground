8fbbbe740f0dd77ba886039aaae3bf6b248de419
Gabor Fekete <gabor@zedge.net>
2015-06-02 11:39:16 +0300

    Added TestApp.

---
+++ TestApp/app/src/main/java/com/feketga/testapp/MainActivity.java
@@ -0,0 +1,80 @@
+package com.feketga.testapp;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                testDirs();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void testDirs() {
+        Context context = getApplicationContext();
+
+        dumpDir(context.getCacheDir(), "context.getCacheDir()");
+        dumpDir(context.getDir("privateTestFile", MODE_PRIVATE), "context.getDir(\"privateTestFile\", MODE_PRIVATE)");
+        dumpDir(context.getFilesDir(), "context.getFilesDir()");
+        dumpDir(context.getDatabasePath("testDbase"), "context.getDatabasePath(\"testDbase\")");
+        dumpDir(Environment.getExternalStorageDirectory(), "Environment.getExternalStorageDirectory()");
+        dumpDir(context.getExternalFilesDir(null), "context.getExternalFilesDir(null)");
+        dumpDir(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), "context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)");
+        dumpDir(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            File[] externalFilesDirs = context.getExternalFilesDirs(null);
+            for (File file : externalFilesDirs) {
+                dumpDir(file, "context.getExternalFilesDirs(null)");
+            }
+        }
+    }
+
+    private void dumpDir(File file, String message) {
+        if (file != null) {
+            Log.d("ZIZI", message + ": " + file.getAbsolutePath());
+        }
+    }
+
+}
